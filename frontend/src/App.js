e2bebda9a936decab4c2c4976dc1309c97f62173
Shaz-hash <73599731+Shaz-hash@users.noreply.github.com>
2024-06-10 15:25:24 +0500

    created frontend repo

---
+++ frontend/src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './App.css';
+import SignUp from './SignUp';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Welcome to Our App</h1>
+        <SignUp />
+      </header>
+    </div>
+  );
+}
+
+export default App;
